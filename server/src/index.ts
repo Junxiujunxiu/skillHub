a1ccc5272afbb104b9cf84ab66bafa06ff8957e9
Junxiujunxiu <sparky46465@gmail.com>
2025-05-26 14:20:01 +1200

    Added configuration files and set up express for handling backend logic and imported some files for backend
    1. Installed pluralize
    2. Set up the express for handling backend logic
    3. Imported some backend files for databse schema.
    4. created tsconfig file and added some configuration.

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,13 +1,43 @@
-import express from "express";
-import dotenv from "dotenv";
-import bodyParser from "body-parser";
-import cors from "cors";
-import helmet from "helmet";
-import morgan from "morgan";
-import * as dynamoose from "dynamoose";
+import express from "express";           // Web server framework
+import dotenv from "dotenv";             // Load .env variables
+import bodyParser from "body-parser";    // Parse request bodies
+import cors from "cors";                 // Allow cross-origin requests
+import helmet from "helmet";             // Add security headers
+import morgan from "morgan";             // Logging tool
+import * as dynamoose from "dynamoose";  // DynamoDB ORM
 
 /*ROute imports*/
 
 
 /*CONFIGURATIONS */
+ // Loads variables from `.env` file into `process.env`
 dotenv.config();
+
+const isProduction = process.env.NODE_ENV ==="production";
+
+if(!isProduction){
+    dynamoose.aws.ddb.local();// Use local DynamoDB for development/testing
+}
+
+const app = express();
+app.use(express.json()); // Parse JSON request bodies
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({policy: "cross-origin"}));
+app.use(morgan("common")); // Logs requests to the console
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(cors());
+
+/*ROUTES*/
+app.get("/", (req, rest) =>{
+    rest.send("Hello World") //// When you visit localhost:3000/, it shows this
+})
+
+/*SERVER*/
+//If you're in development (!isProduction), the server runs on the specified port.
+const port =process.env.PORT || 3000;
+if(!isProduction){
+    app.listen(port, ()=>{
+        console.log(`Server running on port ${port}`);
+    });
+}
