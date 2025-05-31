e21ea396e15ae3f72c28ba861023b4841af5a735
Junxiujunxiu <sparky46465@gmail.com>
2025-06-01 00:59:55 +1200

    Added controller for the course .
    Added course controller logics that send back the response based on the client request.
    it pulls out value either from query or param

---
+++ server/src/controllers/courseController.ts
@@ -0,0 +1,39 @@
+import {Request, Response } from "express";
+import Course from "../models/courseModel";
+
+export const listCourses = async(
+    req: Request,
+    res: Response
+): Promise<void> => {
+    const {category} = req.query;
+    try{
+        const courses = category && category !== "all"
+        //scan the Course table and look for category section and find the one equals category variable then run it. 
+        ? await Course.scan("category").eq(category).exec()
+        //give me all the courses
+        : await Course.scan().exec();
+        //res will send the data back to the client and the client can see the data, like you can see it in postman.
+        res.json({ message: "Courses retrieved successfully", data: courses})
+    }catch (error){
+        res.status(500).json({message: "Error retreiving courses", error});
+    }
+};
+
+export const getCourse = async(
+    req: Request,
+    res: Response
+): Promise<void> => {
+    const {courseID} = req.params;
+    try{
+        //will try to find and return that one course.
+        const course = await Course.get(courseID);
+        if(!course){
+            res.status(404).json({message: "Course not found"});
+            return;
+        }
+
+        res.json({ message: "Courses retrieved successfully", data: course})
+    }catch (error){
+        res.status(500).json({message: "Error retreiving courses", error});
+    }
+};
