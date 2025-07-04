49c0fcc264e7269ae4ce328a119d2aae28c00bbe
Junxiujunxiu <sparky46465@gmail.com>
2025-07-05 03:00:25 +1200

    Created custom authentication flows.
    Created custom authentication flows in clerk.

--- client/src/components/Signin.tsx
+++ client/src/components/Signin.tsx
@@ -14,8 +14,24 @@ const SignInComponent = () => {
   const isCheckoutPage = searchParams.get("showSignUp") !== null;
   const courseId = searchParams.get("id");
 
-  const signUpUrl = isCheckoutPage ? `/checkout?step=1&id=${courseId}&showSignUp=true`: "/signup"
+  // If the user is on the checkout page, build a sign-up link that keeps their course and step info
+  const signUpUrl = isCheckoutPage ? `/checkout?step=1&id=${courseId}&showSignUp=true`: "/signup";
 
+  //get url to redirect the user after signing in
+const getRedirectedUrl = () => {
+  if(isCheckoutPage){
+    return `/checkout?step=2&id=${courseId}`
+  }
+
+  //extract user type from metadata and check if the user is a teacher then redirect them to the teacher dashboard
+  const userType = user?.publicMetadata?.userType as string;
+  if(userType === "teacher"){
+    return "/teacher/courses";
+  }
+
+  //If the user is not on the checkout page and not a teacher, they are redirected to this url
+  return "user/courses";
+};
   return <SignIn 
           appearance={{
             baseTheme: dark,
@@ -36,7 +52,14 @@ const SignInComponent = () => {
               footerActionLink: "text-primary-750 hover:text-primary-600"
             },
           }}
+          //when the user clicks on the sign up button, they are redirected to the sign up page with this URL
           signUpUrl={signUpUrl}
+          //after successful sign in, the user is redirected to this URL
+          forceRedirectUrl={getRedirectedUrl()}
+          //tells clerk to use the hash routing strategy
+          routing="hash"
+          //user is redirected to the home page after signing out
+          afterSignOutUrl="/"
   />
 }
 
